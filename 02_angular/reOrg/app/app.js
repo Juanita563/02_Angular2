6290e3e2a2c249fd2ba58832d73bee5dc13db319
Juanita563 <jlanderson563@gmail.com>
2017-05-04 20:53:48 -0400

    first commit

---
+++ 02_angular/reOrg/app/app.js
@@ -0,0 +1,11 @@
+var recipesApp = angular.module("recipesApp", [ "ngRoute" ]);
+
+recipesApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/recipes",  { 
+        	controller: "RecipeListController",
+            templateUrl: "/app/partials/recipe_list.html" 
+        })
+        .when("/",  { redirectTo: "/recipes" })
+        .otherwise({ redirectTo: "/404_page" });
+});
---
+++ 02_angular/reOrg/app/controller/recipelistcontroller.js
@@ -0,0 +1,27 @@
+(function () {
+
+	function RecipeListController ($scope) {
+	        $scope.new_recipe = { };
+	    
+	        $scope.recipes = [
+	            { name: 'Pasta al Pesto',
+	              type: 'Italian',
+	              summary: 'A quick and simple dish for any Italian household.' },
+	            { name: 'Garlic Pork',
+	              type: 'Thai',
+	              summary: 'A quick and simple meat dish for any Thai table.' },
+	            { name: 'Garlic Broccoli',
+	              type: 'Chinese',
+	              summary: 'Healthy and full of flavour, a simple dish.' },
+	            { name: 'Black Pepper Beef',
+	              type: 'Chinese',
+	              summary: 'Delicious and rich in flavour. A Chinese classic.' }
+	        ];
+	        $scope.addRecipe = function (recipe_data) {
+	            $scope.recipes.push(recipe_data);
+	            $scope.new_recipe = { };
+	        };
+	    }
+
+	    recipesApp.controller("RecipeListController", ['$scope', RecipeListController ]);
+})();
